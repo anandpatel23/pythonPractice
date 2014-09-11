b1aa433b121d784287cf8e7d7a49c199646f97bb
anandpatel23 <ap947@drexel.edu>
2014-09-10 22:55:51 -0400

    adding first .py

---
+++ now.py
@@ -0,0 +1,6 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from datetime import datetime
+now = datetime.now()
+
+print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
