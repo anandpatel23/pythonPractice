cafb49897913ed0ee607c9ef9c9e99a9c7a0fe20
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:29:30 -0400

    Update zipPractice.py

--- zipPractice.py
+++ zipPractice.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> list_a = [3, 9, 17, 15, 19]
+list_a = [3, 9, 17, 15, 19]
 list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
 
 for a, b in zip(list_a, list_b):
