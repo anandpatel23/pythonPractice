71d7fbe975b4bafccaec1028aa533b68f07de94a
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:29:57 -0400

    Update combineLists.py

--- combineLists.py
+++ combineLists.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
 
 def flatten (lists):
     result = []
