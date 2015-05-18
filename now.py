86025e0c6c05c3a9d00a0a08651c9eaf756bde47
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:30:53 -0400

    Update now.py

--- now.py
+++ now.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> from datetime import datetime
+from datetime import datetime
 now = datetime.now()
 
 print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
