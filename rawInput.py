e55ce617a09079f8d31ce1c1aac87686dcce6d66
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:31:34 -0400

    Update rawInput.py

--- rawInput.py
+++ rawInput.py
@@ -1,5 +1,3 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
+while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
     choice = raw_input("Sorry, I didn't catch that. Enter again: ")
 
