de9e87af8204ee3b410696b71d785e3bab16a58c
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:32:29 -0400

    Update whileElse2.py

--- whileElse2.py
+++ whileElse2.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> from random import randint
+from random import randint
 
 # Generates a number from 1 through 10 inclusive
 random_number = randint(1, 10)
