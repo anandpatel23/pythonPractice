eb3a9e0e3651ba2f85c2b2f619f4baeb6d63e425
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:30:10 -0400

    Update fruitsDude.py

--- fruitsDude.py
+++ fruitsDude.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
 
 print 'You have...'
 for f in fruits:
