d00202b92158a75043cc1a85fbca366ff65ffc37
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:31:57 -0400

    Update squareList.py

--- squareList.py
+++ squareList.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> start_list = [5, 3, 1, 2, 4]
+start_list = [5, 3, 1, 2, 4]
 square_list = []
 
 # Your code here!
