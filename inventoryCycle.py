03bfb9f5a0d7fe7db3ff535005f6badcd13d637d
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:30:39 -0400

    Update inventoryCycle.py

--- inventoryCycle.py
+++ inventoryCycle.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> inventory = {
+inventory = {
     'gold' : 500,
     'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
     'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
