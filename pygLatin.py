d6c255e59738f72ca89cb26e134726b33ac07666
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:31:23 -0400

    Update pygLatin.py

--- pygLatin.py
+++ pygLatin.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> pyg = 'ay'
+pyg = 'ay'
 
 original = raw_input('Enter a word:')
 
