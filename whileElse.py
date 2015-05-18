c5101ca84e09715b7859c76ab004796652b441f6
Anand Patel <anandpatel23@users.noreply.github.com>
2015-05-18 11:32:18 -0400

    Update whileElse.py

--- whileElse.py
+++ whileElse.py
@@ -1,6 +1,4 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import random
+import random
 
 print "Lucky Numbers! 3 numbers will be generated."
 print "If one of them is a '5', you lose!"
